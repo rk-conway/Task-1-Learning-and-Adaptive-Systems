7c3d2913d9d2ba548e8295d83520aac5e464294e
= <ronoconway19@gmail.com>
2026-05-01 20:02:46 +0300

    added visualized output for mninst using matplotlib

--- mnist.py
+++ mnist.py
@@ -1,8 +1,9 @@
-# Import libraries
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+import numpy as np
 
-# Load MNIST dataset (digits 0–9)
+# initialize dataset
 (x_train, y_train), (x_test, y_test) = datasets.mnist.load_data()
 
 # Normalize pixel values (0–255 to 0–1)
@@ -30,3 +31,28 @@ model.fit(x_train, y_train, epochs=5)
 test_loss, test_acc = model.evaluate(x_test, y_test)
 
 print("Test accuracy:", test_acc)
+
+"""## Visualizing the results"""
+
+#Get predictions for the test set
+predictions = model.predict(x_test)
+
+#Set up a grid for plotting
+plt.figure(figsize=(10, 10))
+
+for i in range(25):
+    plt.subplot(5, 5, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+
+    plt.imshow(x_test[i], cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions[i])
+    true_label = y_test[i]
+    color = 'green' if predicted_label == true_label else 'red'
+
+    plt.xlabel(f"Pred: {predicted_label} (Actual: {true_label})", color=color)
+
+plt.tight_layout()
+plt.show()
